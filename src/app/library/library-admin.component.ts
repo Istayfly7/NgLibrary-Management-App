db2caeb645d62c722af90d669f61e4e92ad0891d
Istayfly7 <porter_rico@yahoo.com>
2022-03-04 12:51:18 -0700

    adding routing to other nav bar pages

---
+++ src/app/library/library-admin.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'library-admin',
+    template: `
+        <div>
+            <span>Admin Page</span>
+        </div>
+        `
+})
+export class LibraryAdminComponent {
+
+}
---
+++ src/app/library/library-catalog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'library-catalog',
+    template: `
+        <div>
+            <span>Catalog Page</span>
+        </div>
+        `
+})
+export class LibraryCatalogComponent {
+
+}
