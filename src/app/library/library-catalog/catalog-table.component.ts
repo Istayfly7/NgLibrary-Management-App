59306bc02978996801880cc65ee6afed3e342b2e
Istayfly7 <porter_rico@yahoo.com>
2022-03-04 18:30:19 -0700

    adding basic library catlog component

--- src/app/library/library-catalog/catalog-table.component.ts
+++ src/app/library/library-catalog/catalog-table.component.ts
@@ -5,5 +5,21 @@ import { Component } from "@angular/core";
     templateUrl: 'catalog-table.component.html'
 })
 export class CatalogTableComponent {
-
+    books = [
+        {
+            id: 1,
+            author: "J.K. Rowling",
+            title: "Harry Potter and The Sorcerer's Stone"
+        },
+        {
+            id: 2,
+            author: "Carl Dueker",
+            title: "Night Hoops"
+        },
+        {
+            id: 3,
+            author: "Anybody",
+            title: "Random Book"
+        }
+    ];
 }
